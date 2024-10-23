eecbafea62ba104ff90a96c3256fdb43f7640d43
sshekhar3 <Sunny_Shekhar@intuit.com>
2024-10-23 10:26:52 +0530

    02. Added Movie Catalog Service API

--- src/main/java/io/tribe/movie_catalog_service/resources/MovieCatalogResource.java
+++ src/main/java/io/tribe/movie_catalog_service/resources/MovieCatalogResource.java
@@ -1,2 +1,22 @@
-package io.tribe.movie_catalog_service.resources;public class MovieCatalogResource {
+package io.tribe.movie_catalog_service.resources;
+
+import io.tribe.movie_catalog_service.models.CatalogItem;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+    
+    @GetMapping("/{userId}")
+    public List<CatalogItem> getCatalogList(@PathVariable("userId") String userId ) {
+        return Collections.singletonList(
+                new CatalogItem("John", "Wick", 9)
+        );
+    }
 }
